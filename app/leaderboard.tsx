7e0ef17382b6f6c782bd97a543db4204da327509
John <codecreator127@gmail.com>
2023-12-13 21:32:20 +1300

    added firebase storage and firebase firestore for leaderboard info storage, linked it to the table

--- app/leaderboard.tsx
+++ app/leaderboard.tsx
@@ -1,35 +1,46 @@
-export default function Leaderboard() {
+'use client'
+import { db } from '../firebase';
+import { useState, useEffect } from 'react';
+
+
+const Leaderboard = () => {
+
+    const [leaderboardData, setLeaderboardData] = useState<any[]>([]);;
+
+    useEffect(() => {
+        const fetchLeaderboard = async () => {
+            try {
+                const leaderboardSnapshot = await db.collection('leaderboard').get();
+                const data = leaderboardSnapshot.docs.map(doc => doc.data());
+                setLeaderboardData(data);
+            } catch (error) {
+                console.log('Error fetching leaderboard data:', error);
+                return null;
+            }
+        }
+
+        fetchLeaderboard();
+    }, [])
+
     return (
         <div className="content-normal items-center p-20"> 
             <table>
                 <tr>
                     <th>Leaderboard</th>
                 </tr>
-
-                <tr>
-                    <td>First (insert image here later)</td>
-                    <td>Score (link to firebase later)</td>
-                </tr>
-
-                <tr>
-                    <td>Second (insert image here later)</td>
-                    <td>Score (link to firebase later)</td>
-                </tr>
-                
-                <tr>
-                    <td>Third (insert image here later)</td>
-                    <td>Score (link to firebase later)</td>
-                </tr>
-            
+                <tbody>
+                    {leaderboardData.map(item => (
+                        <tr key={"leaderboardData"}>
+                        <td><img src={item.image} width={20} height={20}></img></td>
+                        <td>{item.rank}</td>
+                        <td>{item.score}</td>
+                        </tr>
+                    ))}
+                    </tbody>
             </table>
         
         </div>
-
-
-    
-    
-    
     )
-
-
 }
+
+export default Leaderboard;
