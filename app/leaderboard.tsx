97afa6d107cb06ea776d27c35e5e32f6c166921e
John <codecreator127@gmail.com>
2023-12-13 20:24:55 +1300

    basic leaderboard set up

---
+++ app/leaderboard.tsx
@@ -0,0 +1,35 @@
+export default function Leaderboard() {
+    return (
+        <div className="content-normal items-center p-20"> 
+            <table>
+                <tr>
+                    <th>Leaderboard</th>
+                </tr>
+
+                <tr>
+                    <td>First (insert image here later)</td>
+                    <td>Score (link to firebase later)</td>
+                </tr>
+
+                <tr>
+                    <td>Second (insert image here later)</td>
+                    <td>Score (link to firebase later)</td>
+                </tr>
+                
+                <tr>
+                    <td>Third (insert image here later)</td>
+                    <td>Score (link to firebase later)</td>
+                </tr>
+            
+            </table>
+        
+        </div>
+
+
+    
+    
+    
+    )
+
+
+}
