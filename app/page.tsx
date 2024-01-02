debbe7afdf6d61a0b616d82f986ba05902c4f0dd
John <codecreator127@gmail.com>
2024-01-02 21:57:54 +1300

    minor patch, made components side by side also removed favicon

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import FruitCycle from './fruit-cycle'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="flex min-h-screen justify-center items-start p-24">
       <Leaderboard></Leaderboard>
       <FruitCycle></FruitCycle>
 
