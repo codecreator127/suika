7aed1d45a01bdc13a2fecf9be7e156d0086064bd
John <codecreator127@gmail.com>
2024-01-05 12:41:10 +1300

    added ability to add fruit to canvas on mouse click

--- app/gamearea.tsx
+++ app/gamearea.tsx
@@ -16,6 +16,7 @@ import PeachImage from '../public/assets/peach.png';
 import PineappleImage from '../public/assets/pineapple.png';
 import MelonImage from '../public/assets/melon.png';
 import WatermelonImage from '../public/assets/watermelon.png';
+import { Yellowtail } from 'next/font/google';
 
 
 
@@ -33,17 +34,17 @@ class Fruit {
     }
   }
   
-  const Cherries = new Fruit("Cherry", 2, CherryImage, 20);
-  const Strawberry = new Fruit("Strawberry", 4, StrawberryImage, 25);
-  const Grapes = new Fruit("Grapes", 6, GrapeImage, 35);
-  const Dekopon = new Fruit("Dekopon", 8, DekoponImage, 40);
-  const Orange = new Fruit("Orange", 10, OrangeImage, 45);
-  const Apple = new Fruit("Apple", 12, AppleImage, 50);
-  const Pear = new Fruit("Pear", 14, PearImage, 55);
-  const Peach = new Fruit("Peach", 16, PeachImage, 60);
-  const Pineapple = new Fruit("Pineapple", 18, PineappleImage, 65);
-  const Melon = new Fruit("Melon", 20, MelonImage, 70);
-  const Watermelon = new Fruit("Watermelon", 22, WatermelonImage, 80);
+  const Cherries = new Fruit("Cherry", 2, CherryImage, 25.5);
+  const Strawberry = new Fruit("Strawberry", 4, StrawberryImage, 30);
+  const Grapes = new Fruit("Grapes", 6, GrapeImage, 40);
+  const Dekopon = new Fruit("Dekopon", 8, DekoponImage, 50);
+  const Orange = new Fruit("Orange", 10, OrangeImage, 55);
+  const Apple = new Fruit("Apple", 12, AppleImage, 70);
+  const Pear = new Fruit("Pear", 14, PearImage, 77);
+  const Peach = new Fruit("Peach", 16, PeachImage, 80);
+  const Pineapple = new Fruit("Pineapple", 18, PineappleImage, 88);
+  const Melon = new Fruit("Melon", 20, MelonImage, 92);
+  const Watermelon = new Fruit("Watermelon", 22, WatermelonImage, 100);
 
 let Fruit_Data = [
     Cherries,
@@ -65,7 +66,7 @@ const GameArea = () => {
     console.log('rendered');
     // initial set up
     const engine = Matter.Engine.create();
-    
+
     // game area walls
     const leftWall = Matter.Bodies.rectangle(15, 395, 30, 790, {
       isStatic: true,
@@ -101,41 +102,6 @@ const GameArea = () => {
     const fruit = Fruit_Data[1];
     // console.log(fruit);
 
-    // image loader for fruit bodies
-    const loadImage = (url: string, onSuccess: any, onError: any) => {
-      const img = new Image();
-      img.onload = () => {
-        onSuccess(img.src);
-      };
-      img.onerror = onError();
-      img.src = url;
-    };
-
-    loadImage(
-      CherryImage.src,
-      (      url: any) => {
-        console.log("Success");
-        Matter.World.add(engine.world, [
-          Matter.Bodies.circle(300, 300, 25, {
-            // density: 0.0005,
-            // frictionAir: 0.06,
-            // restitution: 0.3,
-            // friction: 0.01,
-            render: {
-              sprite: {
-                xScale: 1,
-                yScale: 1,
-                texture: url // set texture here
-              }
-            }
-          })
-        ]);
-      },
-      () => {
-        console.log("Error  Loading ");
-      }
-    );
-
 
     // Render the engine
     const render = Matter.Render.create({
@@ -159,33 +125,85 @@ const GameArea = () => {
     let gameOver = false;
 
     const runner = Matter.Runner.create();
+
+    // image loader for fruit bodies
+    const loadImage = (url: string, onSuccess: any, onError: any) => {
+      const img = new Image();
+      img.onload = () => {
+        onSuccess(img.src);
+      };
+      img.onerror = onError();
+      img.src = url;
+    };
     
     // chance of bigger fruit spawning
-    let fruit_multiplier = 1;
+    let fruit_multiplier = 5;
 
-    function addFruit() {
+    function addFruit(x:number, y: number) {
       const fruitIndex = Math.floor(Math.random() * fruit_multiplier)
 
       // import image
       const fruit = Fruit_Data[fruitIndex];
       // console.log(fruit);
 
-      const game_fruit = Matter.Bodies.circle(300, 50, fruit.radius, {
-        // index: fruitIndex,
-        isSleeping: true,
+      const game_fruit = Matter.Bodies.circle(x, y, fruit.radius, {
+        // density: 0.0005,
+        // frictionAir: 0.06,
+        // restitution: 0.3,
+        // friction: 0.01,
         render: {
-          sprite: {xScale: 300, yScale: 40, texture: `${fruit.image_url}`}
+          sprite: {
+            xScale: 1,
+            yScale: 1,
+            texture: fruit.image_url.src // set texture here
+          }
         }
-
-      })
+      });
+      
+      loadImage(
+        fruit.image_url.src,
+        () => {
+          console.log("Success");
+          Matter.World.add(engine.world, game_fruit);
+        },
+        () => {
+          console.log("Error  Loading ");
+        }
+      );
       currentBody = game_fruit;
       currentFruit = fruit;
-      Matter.World.add(engine.world, game_fruit);
     }
 
-    // addFruit();
+    addFruit(300, 300);
 
-    // Start the engine
+    // click to add fruit
+    const mouse = Matter.Mouse.create(render.canvas);
+
+    // Create a mouse constraint
+    const mouseConstraint = Matter.MouseConstraint.create(engine, {
+      mouse: mouse,
+      constraint: {
+        stiffness: 0.2,
+        render: {
+          visible: false,
+        },
+      },
+    });
+
+    // Add the mouse constraint to the world
+    Matter.World.add(engine.world, mouseConstraint);
+
+    // Listen for mouse clicks
+    Matter.Events.on(mouseConstraint, 'mousedown', (event) => {
+      // Handle mouse click event here
+      console.log('Mouse clicked at:', event.mouse.position);
+
+      addFruit(event.mouse.position.x, event.mouse.position.y)
+
+      
+    });
+
+    // Start the physics engine
     Matter.Runner.run(runner, engine);
 
     // Clean up on component unmount
